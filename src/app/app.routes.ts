0f22cdd30d8b522bf21a6b6378de5e3b5860179b
Ariel Ayala <arielayalamail@gmail.com>
2025-02-25 09:24:12 -0300

    Delete unused home component/page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'calendar',
     loadComponent: () => import('./pages/calendar/calendar.page')
   },
   {
