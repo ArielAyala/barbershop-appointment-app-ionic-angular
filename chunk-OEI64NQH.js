f710ee83c740a5742789812751397cced6896da9
ArielAyala <arielayalamail@gmail.com>
2025-02-27 13:10:05 +0000

    Deploying to gh-pages from @ ArielAyala/barbershop-appointment-app-ionic-angular@aa4bfb7ffa8945eb5bbd1ea309cc60470a4cb994 🚀

---
+++ chunk-OEI64NQH.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-PP7MKLMN.js";import"./chunk-IZ3NJ2KX.js";import"./chunk-G3CV3VGG.js";import"./chunk-MNDBORQJ.js";import"./chunk-7QVI4S4L.js";import"./chunk-BTS5SJXR.js";import"./chunk-LF5XB4YN.js";import"./chunk-LNJ3S2LQ.js";export{b as iosTransitionAnimation,a as shadow};
